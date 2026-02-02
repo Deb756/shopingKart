04590a10aa243bb06df865eddc58994ce58af662
Debabrat <123225627+Deb756@users.noreply.github.com>
2026-02-02 10:41:00 +0530

    thymeleaf added

--- src/main/java/com/org/shopingKart/Service/productService.java
+++ src/main/java/com/org/shopingKart/Service/productService.java
@@ -5,8 +5,15 @@ import com.org.shopingKart.repository.productRepo;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class productService {
@@ -18,12 +25,25 @@ public class productService {
         return productRepo.findAll();
     }
 
-    public Product save(Product product) {
-        double discountAmount =
-                product.getPrice() * (product.getDiscount() / 100.0);
 
+    public Product save(Product product, MultipartFile imageFile) {
+        String fileName = UUID.randomUUID() + "_" + imageFile.getOriginalFilename();
+        Path uploadPath = Paths.get("uploads/products");
+        try {
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(imageFile.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+//            setting image path in product
+            product.setImage("/uploads/products/" + fileName);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to store image", e);
+        }
+
+//        discount calculation
+        double discountAmount = product.getPrice() * (product.getDiscount() / 100.0);
         product.setDiscountPrice(product.getPrice() - discountAmount);
-
         return productRepo.save(product);
     }
 
@@ -31,8 +51,7 @@ public class productService {
         return productRepo.findById(id).orElse(null);
     }
 
-    public Product update(Product product, ObjectId id) {
-
+    public Product update(Product product, ObjectId id, MultipartFile imageFile) {
         Product existingProduct = productRepo.findById(id).orElse(null);
         if (existingProduct == null) return null;
 
@@ -45,35 +64,78 @@ public class productService {
         if (product.getCategory() != null)
             existingProduct.setCategory(product.getCategory());
 
+        if (product.getStock() != null)
+            existingProduct.setStock(product.getStock());
+
         if (product.getPrice() != null)
             existingProduct.setPrice(product.getPrice());
 
         if (product.getDiscount() != null)
             existingProduct.setDiscount(product.getDiscount());
 
-        if (product.getPrice() != null) {
-            double discountPrice =
-                    Math.round(
-                            product.getPrice() *
-                                    (1 - product.getDiscount() / 100.0) * 100
-                    ) / 100.0;
+        if (imageFile != null && !imageFile.isEmpty()) {
+            try {
+                String filename = imageFile.getOriginalFilename();
+                Files.copy(imageFile.getInputStream(), Paths.get("uploads/products" + filename), StandardCopyOption.REPLACE_EXISTING);
+                existingProduct.setImage("/uploads/products/" + filename);
 
-            existingProduct.setDiscountPrice(discountPrice);
-        }
-        // Recalculate discount price if needed
-        if (product.getDiscount() != null) {
-            double discountPrice = Math.round(existingProduct.getPrice() * (1 - product.getDiscount() / 100.0) * 100) / 100.0;
-
-            existingProduct.setDiscountPrice(discountPrice);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to update image", e);
+            }
         }
 
+        if (product.getPrice() != null || product.getDiscount() != null) {
+            double price = existingProduct.getPrice();
+            int discount = existingProduct.getDiscount() != null ? existingProduct.getDiscount() : 0;
+
+            double discountPrice = Math.round(price * (1 - discount / 100.0) * 100) / 100.0;
+            existingProduct.setDiscountPrice(discountPrice);
+        }
         return productRepo.save(existingProduct);
     }
+
 
     public Boolean delete(ObjectId id) {
         if (!productRepo.existsById(id)) return false;
         productRepo.deleteById(id);
         return true;
     }
+
+    public List<Product> getProductsByCategory(String category) {
+        return productRepo.findAllByCategory(category);
+    }
+
+    public Product updateStock(ObjectId productId, Integer newStock) {
+        if (newStock == null) return null;
+
+        Product prod = productRepo.findById(productId).orElse(null);
+        if (prod == null) return null;
+
+        int currentStock = prod.getStock() != null ? prod.getStock() : 0;
+        int updateStock = currentStock + newStock;
+
+        if (newStock < 0) {
+            throw new IllegalArgumentException("Stock cannot be negative");
+        }
+
+        prod.setStock(updateStock);
+        return productRepo.save(prod);
+    }
+
+    public Product updateDiscount(ObjectId productId, Integer discount) {
+        if (discount < 0 || discount > 100) {
+            throw new IllegalArgumentException("Discount must be between 0 and 100");
+        }
+        Product prod = productRepo.findById(productId).orElse(null);
+        if (prod == null) return null;
+        prod.setDiscount(discount);
+        if (prod.getPrice() != null) {
+            double discountedPrice = prod.getPrice() * (1 - discount / 100.0);
+            discountedPrice = Math.round(discountedPrice * 100.0) / 100.0;
+            prod.setDiscountPrice(discountedPrice);
+        }
+        return productRepo.save(prod);
+    }
+
 
 }
--- src/main/java/com/org/shopingKart/controller/productController.java
+++ src/main/java/com/org/shopingKart/controller/productController.java
@@ -5,11 +5,14 @@ import com.org.shopingKart.entity.Product;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/products")
 public class productController {
@@ -29,21 +32,38 @@ public class productController {
         return product == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(product);
     }
 
-    @PostMapping
-    public ResponseEntity<Product> saveProduct(@RequestBody Product product) {
-        Product prod = productService.save(product);
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Product> saveProduct(@RequestPart("product") Product product, @RequestPart("image") MultipartFile imageFile) {
+        Product prod = productService.save(product, imageFile);
         return prod == null ? ResponseEntity.noContent().build() : ResponseEntity.ok(prod);
     }
 
     @PutMapping("{prodId}")
-    public ResponseEntity<Product> updateProduct(@RequestBody Product product, @PathVariable("prodId") ObjectId prodId) {
-        Product prod = productService.update(product, prodId);
+    public ResponseEntity<Product> updateProduct(@RequestPart("product") Product product, @PathVariable("prodId") ObjectId prodId, @RequestPart(value = "image", required = false) MultipartFile imageFile) {
+        Product prod = productService.update(product, prodId, imageFile);
         return prod == null ? ResponseEntity.noContent().build() : ResponseEntity.ok(prod);
     }
 
     @DeleteMapping
     public ResponseEntity<Boolean> removeProduct(@PathVariable("prodId") ObjectId prodId) {
         return productService.delete(prodId) ? new ResponseEntity<>(HttpStatus.NOT_FOUND) : ResponseEntity.ok(true);
+    }
+
+    @GetMapping("category/{categoryName}")
+    public ResponseEntity<List<Product>> getAllProductByCategory(@PathVariable("categoryName") String category) {
+        return ResponseEntity.ok(productService.getProductsByCategory(category));
+    }
+
+    @PutMapping("stock/{prodId}")
+    public ResponseEntity<Product> updateStocks(@PathVariable("prodId") ObjectId prodId, @RequestParam Integer newStock) {
+        Product product = productService.updateStock(prodId, newStock);
+        return product == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(product);
+    }
+
+    @PutMapping("discount/{prodId}")
+    public ResponseEntity<Product> updateDiscount(@PathVariable("prodId") ObjectId prodId, @RequestParam Integer discount) {
+        Product product = productService.updateDiscount(prodId, discount);
+        return product == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(product);
     }
 
 }
